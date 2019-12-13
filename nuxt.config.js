2f3953551baab79ef7716aa372fc505de98a4b7f
34r7h <2566560+34r7h@users.noreply.github.com>
2019-12-13 12:41:58 +0400

    google console meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,8 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      {name: 'google-site-verification',content:'Yl7o4EiONrzabXO507C9CdewMhmoFl_uxPXJYDBKtPY'}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
